0c164bdab62ed73d2e6ac31b0b2c36e8fbc7186b
ericgaona <ericgaona0413@gmail.com>
2020-05-12 12:34:26 +0000

    prueba 4

--- env.py
+++ env.py
@@ -1,2 +1,4 @@
 import os
 
+os.environ.setdefault("DATABASE_URL", "postgres://yclnuvbowclhpj:393480675bd1d7e842808327062593d94783adc0cb87329340b1fab1ff7471d6@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/d70u3frcbqb311")
+os.environ.setdefault("SECRET_KEY", "hk0#f_(1n4d##%(%y%2+stlx=&_ox-fin0yzxlq@w22!q_27&y")
